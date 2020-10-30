4cc2f4db4e15e7ff7b54eac27e984b0d176f9685
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-10-30 19:18:44 -0300

    feat: generate token JWT

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -9,11 +9,14 @@ sessionsRouter.post('/', async (request, response) => {
     const { email, password } = request.body;
 
     const authenticateUserService = new AuthenticateUserService();
-    const { user } = await authenticateUserService.execute({ email, password });
+    const { user, token } = await authenticateUserService.execute({
+      email,
+      password,
+    });
 
     delete user.password;
 
-    return response.json(user);
+    return response.json({ user, token });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
 
 import User from '../models/User';
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +29,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, 'f48268dea04a3faa1900f17ff7da50df', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
