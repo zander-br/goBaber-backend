8740d02ab402d17bbb36d489bd24d5daf53c777e
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-10-30 05:43:59 -0300

    chore: add reflect-metadata

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,27 +1,26 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
     const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService(
-      appointmentRepository,
-    );
-    const appointment = createAppointmentService.execute({
+    const createAppointmentService = new CreateAppointmentService();
+    const appointment = await createAppointmentService.execute({
       provider,
       date: parsedDate,
     });
