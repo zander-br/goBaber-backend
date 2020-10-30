6a7626a6f2121e01706f3f4773495f244e593ad2
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-10-30 06:47:13 -0300

    feat: add migration alter provider table

--- src/database/migrations/1604048985635-CreateUsers.ts
+++ src/database/migrations/1604048985635-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1604048985635 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
