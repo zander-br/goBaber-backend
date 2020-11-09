cfc3eed69227aa9296b7ffeab84f91859c820210
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-09 18:27:01 -0300

    test: should not be able to authenticate with non existing user

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import { AuthorizationError } from '@shared/errors';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -29,5 +30,20 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = { email: 'johndoe@example.com', password: '123456' };
+
+    await expect(authenticateUser.execute(user)).rejects.toBeInstanceOf(
+      AuthorizationError,
+    );
   });
 });
