2e3b79c1eba8187b1c8afda7a0c514e4a7d8961f
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-11 06:10:56 -0300

    feat: implementation send mail in development

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,7 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   tmpFolder,
-  uploadsFilder: path.resolve(tmpFolder, 'uploads'),
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
