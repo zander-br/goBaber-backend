66edcac7c86822d6666cd4f22589674a01e2a906
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-11 19:39:47 -0300

    feat: should be able to update the profile

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,40 @@
+import { AppError } from '@shared/errors';
+import { inject, injectable } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UsersRepository')
+    private readonly usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private readonly hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, name, email }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfileService;
