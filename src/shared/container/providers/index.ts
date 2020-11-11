b41ef5499d3ac80970d7cb8ed2d3d06d051acd42
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-11 17:36:40 -0300

    feat: add MailTemplateProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,9 +2,11 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -14,4 +16,9 @@ container.registerSingleton<IStorageProvider>(
 container.registerInstance<IMailProvider>(
   'MailProvider',
   new EtherealMailProvider(),
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
 );
