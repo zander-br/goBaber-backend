8565bcccc0ebbddb1e11cd69e96e8568e2a3930c
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-11 04:32:32 -0300

    test: should not be able to reset password if passed more than 2 hours

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -56,4 +56,23 @@ describe('ResetPassword', () => {
       resetPassword.execute({ token, password: '123456' }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should not be able to reset password if passed more than 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      const customDate = new Date();
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPassword.execute({ password: '123123', token }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
