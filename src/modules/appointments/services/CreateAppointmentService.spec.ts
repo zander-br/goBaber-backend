e471d9dd54dbebb4442535d9d5d1c0713d489f9a
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-20 06:17:38 -0300

    test: should not be able to create an appointment before 8am

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -30,7 +30,11 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 11, 9, 4);
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    const appointmentDate = new Date(2020, 4, 10, 13);
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -70,6 +74,20 @@ describe('CreateAppointment', () => {
       createAppointment.execute({
         date: new Date(2020, 4, 10, 13),
         user_id: 'provider_id',
+        provider_id: 'provider_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment before 8am', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 7),
+        user_id: 'user_id',
         provider_id: 'provider_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
