9a6d1ad127ccc5e845e4a6b93ef1701454768f34
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-23 04:51:35 -0300

    feat: add support aws ses

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: process.env.MAIL_DEFAULT_FROM,
+      name: process.env.MAIL_DEFAULT_NAME,
+    },
+  },
+} as IMailConfig;
